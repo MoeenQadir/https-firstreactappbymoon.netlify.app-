6e20ed7f2b224cb7400b3027196ff542d31c6b9b
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2023-01-23 18:26:09 -0800

    ll

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo  from '../assets/logo.png'
+import lll  from '../assets/logo.png'
 function Nav() {
     return(
         <div>
@@ -9,7 +9,7 @@ function Nav() {
                         className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
                         <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
                            href="#pablo">
-                            <img src={Logo} style={{height:'50px', width:'80px'}} alt={"jsa"}></img>
+                            <img src={lll} style={{height:'50px', width:'80px'}} alt={"jsa"}></img>
                         </a>
                         <button
                             className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
